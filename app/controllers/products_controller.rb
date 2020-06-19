87f25a2e2e172e8826be88e054cbf79e9ddca953
kr95656 <katmsg8239@gmail.com>
2020-06-19 11:02:13 +0900

    Fix partial template flle name

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    
+    @products = [
+      {
+        name: "name1",
+        price: 3000,
+        des: "a"
+    }
+  ]
+                        
   end
 end
